ee15385607a4eb093ba9948d61998fe7ba942fbd
arya-abadi <arya.abadi89@gmail.com>
2025-07-09 12:32:40 +0200

    add: mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,14 +25,19 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClerkProvider>
-            <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
             >
-            {children}
-            </body>
-            </html>
+                {children}
+            </ThemeProvider>
         </ClerkProvider>
+        </body>
+        </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button"
+import {ModeToggle} from "@/components/ModeToggle/ModeToggle";
 
 export default function Home() {
   return (
@@ -6,19 +8,16 @@ export default function Home() {
         <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
                 <SignInButton mode="modal">
-                    <button className="bg-green-500 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                        Sign In
-                    </button>
+                    <Button variant="outline">Sign In</Button>
                 </SignInButton>
                 <SignUpButton>
-                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                        Sign Up
-                    </button>
+                    <Button variant="secondary">Sign Up</Button>
                 </SignUpButton>
             </SignedOut>
             <SignedIn>
                 <UserButton />
             </SignedIn>
+            <ModeToggle/>
         </header>
     </div>
   );
