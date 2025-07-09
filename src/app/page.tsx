e8f6fe93d2fab4e6d55d72f24632d43a84012aea
arya-abadi <arya.abadi89@gmail.com>
2025-07-09 11:27:43 +0200

    add: clerk sign in and sign out components in the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello Home Page</h1>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+                <SignInButton mode="modal">
+                    <button className="bg-green-500 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign In
+                    </button>
+                </SignInButton>
+                <SignUpButton>
+                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign Up
+                    </button>
+                </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </header>
     </div>
   );
 }
