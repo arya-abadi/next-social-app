babdcdf29ce5d7de21ab8c25f10bb8923e805db6
arya-abadi <arya.abadi89@gmail.com>
2025-07-11 10:58:40 +0200

    add: implement like, comment and delete posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 import { currentUser } from "@clerk/nextjs/server";
 import CreatePost from "@/components/CreatePost/CreatePost";
 import WhoToFollow from "@/components/WhoToFollow/WhoToFollow";
+import {getPosts} from "@/actions/post.action";
+import {getDbUserId} from "@/actions/user.action";
+import PostCard from "@/components/PostCard/PostCard";
 
 export default async function Home() {
     const user = await currentUser();
-    // const posts = await getPosts();
-    // const dbUserId = await getDbUserId();
+    const posts = await getPosts();
+    const dbUserId = await getDbUserId();
+
+    console.log("posts", posts);
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
             <div className="lg:col-span-6">
                 {user ? <CreatePost /> : null}
 
-                {/*<div className="space-y-6">*/}
-                {/*    {posts.map((post) => (*/}
-                {/*        <PostCard key={post.id} post={post} dbUserId={dbUserId} />*/}
-                {/*    ))}*/}
-                {/*</div>*/}
+                <div className="space-y-6">
+                    {posts.map((post) => (
+                        <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+                    ))}
+                </div>
             </div>
 
             <div className="hidden lg:block lg:col-span-4 sticky top-20">
